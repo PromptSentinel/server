45dc90073a3628026cc61234ab04e7609a671b45
Nevaeh0701 <162300874+Nevaeh0701@users.noreply.github.com>
2025-04-08 20:02:28 +0900

    initial commit

---
+++ src/main/java/com/example/promptsentinel/PromptsentinelApplication.java
@@ -0,0 +1,13 @@
+package com.example.promptsentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptsentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptsentinelApplication.class, args);
+	}
+
+}
