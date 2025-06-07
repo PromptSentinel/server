abfd1c8304a1e2aac2d0016dcf71aae0045d2f87
Nevaeh0701 <162300874+Nevaeh0701@users.noreply.github.com>
2025-06-07 23:53:49 +0900

    csvdata 필드 수정

--- src/main/java/com/example/promptsentinel/domain/evalutation/Evaluation.java
+++ src/main/java/com/example/promptsentinel/domain/evalutation/Evaluation.java
@@ -2,6 +2,7 @@ package com.example.promptsentinel.domain.evalutation;
 
 import com.example.promptsentinel.domain.member.entity.Member;
 import com.example.promptsentinel.domain.model.entity.LLMModel;
+import com.example.promptsentinel.global.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Evaluation {
+public class Evaluation extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
