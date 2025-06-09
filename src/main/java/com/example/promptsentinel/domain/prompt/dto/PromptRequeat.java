dcd0921cd80fde6433023dbc9e5a27384527046c
Nevaeh0701 <162300874+Nevaeh0701@users.noreply.github.com>
2025-06-10 01:46:28 +0900

    prompt 저장 로직 구현

---
+++ src/main/java/com/example/promptsentinel/domain/prompt/dto/PromptRequeat.java
@@ -0,0 +1,11 @@
+package com.example.promptsentinel.domain.prompt.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PromptRequeat {
+    private String staregy;
+    private String prompt;
+}
--- src/main/java/com/example/promptsentinel/domain/prompt/service/PromptService.java
+++ src/main/java/com/example/promptsentinel/domain/prompt/service/PromptService.java
@@ -1,20 +1,41 @@
 package com.example.promptsentinel.domain.prompt.service;
 
-import com.example.promptsentinel.domain.model.dto.LLMResponse;
+import com.example.promptsentinel.domain.evalutation.dto.EvaluationDetailResponse;
+import com.example.promptsentinel.domain.model.entity.LLMModel;
 import com.example.promptsentinel.domain.model.service.LLMClientService;
 import com.example.promptsentinel.domain.prompt.dao.PromptRepository;
+import com.example.promptsentinel.domain.prompt.dto.PromptListRequest;
+import com.example.promptsentinel.domain.prompt.dto.PromptRequeat;
 import com.example.promptsentinel.domain.prompt.entity.Prompt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PromptService {
     private final PromptRepository promptRepository;
+    private final LLMClientService llmClientService;
 
     public List<Prompt> getQuestion(){
         return promptRepository.findAll();
     }
+
+    public List<Prompt> uploadPrompt(PromptListRequest promptListRequest) {
+        return promptListRequest.getPromptRequeatList().stream()
+                .map(promptRequeat -> promptRepository.save(
+                        Prompt.builder()
+                                .strategy(promptRequeat.getStaregy())
+                                .question(promptRequeat.getPrompt())
+                                .build()))
+                .collect(Collectors.toList());
+    }
+
+//    public EvaluationDetailResponse getResult(LLMModel llmModel){
+//        List<Prompt> promptList = getQuestion();
+//
+//    }
 }
